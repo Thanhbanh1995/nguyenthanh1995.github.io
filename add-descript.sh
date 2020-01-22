f4299e0e1c7b66bd08297e2ecc9eab8ed10f1123
NguyenThanh1995 <45375496+NguyenThanh1995@users.noreply.github.com>
2020-01-23 06:33:33 +0700

    Add files via upload

--- add-descript.sh
+++ add-descript.sh
@@ -2,18 +2,25 @@
 
 for i in ./debs/*.deb
 do
-   rm -fr ./debs/tmp
-   pkg=`dpkg -f $i`
-   pkg=`echo "$pkg" | grep "Package: " | cut -c 10- | tr -d "\n\r"`
-
+   rm -rf ./debs/tmp
+   info=`dpkg -f $i`
+   dep=`echo "$info" | grep "Depiction: " | cut -c 12- | tr -d "\n\r"`
+   home=`echo "$info" | grep "Homepage: " | cut -c 11- | tr -d "\n\r"`
+   
    dpkg-deb -R $i ./debs/tmp
-
-   echo "Depiction: https://nguyenthanh1995.github.io/description.html?goto=${pkg}" >> ./debs/tmp/DEBIAN/control
-   echo "Homepage: https://nguyenthanh1995.github.io/" >> ./debs/tmp/DEBIAN/control
+   if [[ -z $dep ]]; then
+       echo "Depiction: https://nguyenthanh1995.github.io/description.html?goto=${pkg}" >> ./debs/tmp/DEBIAN/control
+   else continue
+   fi
+   if [[ -z $home ]]; then
+        echo "Homepage: https://nguyenthanh1995.github.io/" >> ./debs/tmp/DEBIAN/control
+   else continue
+   fi
 
    dpkg -b ./debs/tmp
 
    bsname=$(basename "$i")
    mv ./debs/tmp.deb "./debs/$bsname"
 done
+   rm -fr ./debs/tmp
 exit 0
